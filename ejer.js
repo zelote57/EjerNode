300f6c951e1daeafe24cffc467e4c13e39ebb37c
Gustavo Lozada <zelote57@gmail.com>
2024-04-29 21:42:53 -0500

    sumar 2 numeros

--- ejer.js
+++ ejer.js
@@ -9,19 +9,34 @@ const server = servidor.createServer((req, resp)=>{
         });
 
         req.on('end', ()=>{
-            console.log(body)
+            let arrayNum = body.split('&');
+            console.log(arrayNum);
+            let num1 = arrayNum[0].split('=')[1];
+            console.log(num1);
+            let num2 = arrayNum[1].split('=')[1];
+            
+            let sum = num1 + num2;
+
+            //console.log(num2);
+            //let num = parseInt(num1);
+            // let numdos = parseInt(num2);
+            // let suma =  num + numdos;
+
+            resp.end(sum);
         })
     }
     else{
         resp.setHeader('Content-Type', 'text/html');
-        resp.end(`<form method="POST>
-                    <input type= "text"
-                    name="num1"/>
+        resp.end(`<form method="POST">
+                    <input name="num1" type="number"/>
                     <br/>
-                    <input type= "text"
-                    name="num2"/>
+                    <br/>
+                    <input name="num2" type="number"/>
+                    <br/>
                     <br/>
                     <button>Calcular</button>
                    </form>`)
     }
 })
+
+server.listen(5001);
